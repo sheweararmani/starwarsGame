fedf74357b83e93d33b8a4ade26ad99182df87a9
Oscar <oscartoomey10@gmail.com>
2018-10-23 10:45:45 +0100

    adding to repos

---
+++ map.py
@@ -0,0 +1,12 @@
+location_alderaan = {
+    "name": "Alderaan",
+    "description": "I'm literally just writing this so I can test the code.",
+    "exits": {
+        "north": "alderaan"
+    },
+    "items": []
+}
+
+locations = {
+    "alderaan" : location_alderaan
+}
