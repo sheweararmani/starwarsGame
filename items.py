dfa2026bc53612c6f73c1e37d67e641e89802259
Oscar <oscartoomey10@gmail.com>
2018-10-23 18:20:36 +0100

    added items and fixed errors

--- items.py
+++ items.py
@@ -1,1 +1,94 @@
-items = []
+item_lightsaber = {
+    "id": "lighsaber",
+
+    "name": "Lightsaber",
+
+    "description":
+    "________________",
+
+    "damage": 15
+}
+
+item_pc = {
+    "id": "powerconverter",
+
+    "name": "Power Converter",
+
+    "description":
+    "________________",
+
+    "damage": 0
+}
+
+item_landspeeder = {
+    "id": "landspeeder",
+
+    "name": "Landspeeder",
+
+    "description":
+    "________________",
+
+    "damage": 0
+}
+
+item_r2d2 = {
+    "id": "r2d2",
+
+    "name": "R2D2",
+
+    "description": "________________",
+
+    "damage": 0
+}
+
+item_handgun = {
+    "id": "pistol",
+    
+    "name": "Blaster Pistol",
+
+    "description": "________________",
+
+    "damage": 5
+    
+}
+
+item_chewie = {
+    "id": "chewie",
+    
+    "name": "Chewbacca",
+
+    "description": "________________",
+
+    "damage": 0
+}
+
+item_leia = {
+	"id": "leia",
+
+	"name": "Princess Leia",
+
+	"description": "________________",
+
+        "damage": 0
+        }
+
+item_blaster = {
+        "id": "blaster",
+
+        "name": "blaster",
+
+        "description": "Standard issue blaster rifle.",
+
+        "damage": 10
+        }
+
+items = {
+    "lighsaber": item_lightsaber,
+    "powerconverter": item_pc,
+    "landspeeder": item_landspeeder,
+    "r2d2": item_r2d2,
+    "handgun": item_handgun,
+    "chewie": item_chewie,
+    "leia": item_leia,
+    "blaster": item_blaster
+}
