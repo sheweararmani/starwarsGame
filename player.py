238e311976d279cba60328533735e1a9d9d1060e
Cameron Bolitho <C1806809@cf.ac.uk>
2018-10-23 11:23:31 +0000

    Oops fixed player dictionaries.

--- player.py
+++ player.py
@@ -3,14 +3,16 @@ from items import *
 
 # TODO: everything in here.
 
-luke = {"inventory" : [], # Luke's items.
-        "max_mass" : 10, # The amount Luke can carry.
-        "location" : location_alderaan, # The location Luke starts in.
+luke = {"inventory": [], # Luke's items.
+        "max_mass": 10, # The amount Luke can carry.
+        "location": location_alderaan, # The location Luke starts in.
+        "health": 100, # Luke's current health.Zz
         "max_health": 100, # Luke's maximum health.
         "damage": 6} # Luke's damage, goes up if picks up weapons
 
-han = {"inventory" : [], # Han's items.
-       "max_mass" : 5, # The amount Han can carry.
-       "location" : {}, # The location Han starts in.
-       "max_health" : 1, # Han's maximum health.
+han = {"inventory": [], # Han's items.
+       "max_mass": 5, # The amount Han can carry.
+       "location": {}, # The location Han starts in.
+       "health": 1, # Han's current health.
+       "max_health": 1, # Han's maximum health.
        "damage": 5} # Han's damage.
