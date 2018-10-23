e13d46c72abe5d2db470861a8d32cbf52bd1d4e0
Oscar <oscartoomey10@gmail.com>
2018-10-23 10:43:45 +0100

    adding to thingy

---
+++ enemies.py
@@ -0,0 +1,23 @@
+import random
+
+stormtrooper = { "name": "stormtrooper",
+                 "description": "Can't shoot for shit",
+                 "health": random.randint(1,10),
+                 "damage": random.randint(1,100),
+                 "items":["blaster","helmet"]
+                 }
+
+
+greedo = { "name": "Greedo",
+           "description":"Who shot first?",
+           "health":50,
+           "damage":20,
+           "items":"password"
+           }
+
+Darth_Kirill = { "name" : "Darth Kirill",
+                 "description": "He is your lecturer.",
+                 "health" : 100,
+                 "damage": 100,
+                 "items": ["lightsaber","mask","cigarettes"]
+                 }
