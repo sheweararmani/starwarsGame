6d02c77c74f773877e9d7f8f343bb384559d0e3f
Oscar <oscartoomey10@gmail.com>
2018-10-24 15:41:09 +0100

    added more detail to the functon and changed function name to fight

--- shoot.py
+++ shoot.py
@@ -1,30 +1,39 @@
 from enemies import *
 from player import *
-from time import clock
-
-killwords = ["bang","pew","shoot","kill"]
+from time import clock,sleep
 
 
-def input_fast_enough(max_time,enemy):
+
+def fight(max_time,enemy):
     # Record the time before and after the user types their input.
-    correct = random.choice(killwords)
-    start_time = clock()
+    print("Enemy: {}".format(enemy["name"]))
+    sleep(1)
     print("Enter the next word quickly!:")
-    user_input = input(correct + "\n")
+    sleep(0.5)
+    print(".")
+    sleep(0.5)
+    print(".")
+    sleep(0.5)
+    print(".")
+    sleep(0.5)
+    print("Fight!")
+    start_time = clock()
     end_time = clock()
     # Check if they were fast enough and gave the correct input.
     time_taken = end_time - start_time
     while True:
+        killwords = enemy["killwords"]
+        correct = random.choice(killwords)
+        user_input = input(correct + "\n")
         if user_input == correct and time_taken <= max_time:
-            enemy["health"] = enemy["health"] - luke["luke_damage"]
+            enemy["health"] = enemy["health"] - luke["damage"]
+            print(enemy["health"])
             if enemy["health"] <= 0:
-                print("enemy is dead")
-                return
+                return True
         else:
-            luke["luke_max_health"] = luke["luke_max_health"] - enemy["damage"]
-            print(luke["luke_max_health"])
-            if luke["luke_max_health"] <= 0:
-                print("luke is dead")
-                return
+            luke["health"] = luke["health"] - enemy["damage"]
+            print(luke["health"])
+            if luke["health"] <= 0:
+                return False
 
-input_fast_enough(3,stormtrooper)
+fight(3,darth_kirill) 
